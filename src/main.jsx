ed2b0c18d9af69ce9ed8537caa8d59e06320ab66
Angel727524 <cheonsa676@gmail.com>
2026-06-19 13:38:55 +0900

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(
+document.getElementById("root")
+).render(
+<React.StrictMode> <BrowserRouter> <App /> </BrowserRouter>
+</React.StrictMode>
 );
